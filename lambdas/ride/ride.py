fe1f8532f7bed1d8d1f034bdd6bddc3104f5b443
ncfakude30 <39239761+ncfakude30@users.noreply.github.com>
2024-09-06 15:32:11 +0200

    ride fix

--- lambdas/ride/ride.py
+++ lambdas/ride/ride.py
@@ -1,14 +1,19 @@
 import os
 import boto3
 from boto3.dynamodb.conditions import Key
+import os
+import boto3 
+
+RECENT_TRIPS_TABLE = os.getenv('RECENT_TRIPS_TABLE', 'TripsTable')
 
 dynamodb = boto3.resource('dynamodb')
-table_name = os.environ.get('RECENT_TRIPS_TABLE')
-table = dynamodb.Table(table_name)
 
 def handler(event, context):
+
+    print(f'My event: {event}')
     user_id = event['queryStringParameters']['userId']
 
+    table = dynamodb.Table(RECENT_TRIPS_TABLE)
     response = table.query(
         KeyConditionExpression=Key('userId').eq(user_id)
     )
