2fe95037fccbb7d20504b6c87d51565f2aa25545
ncfakude30 <39239761+ncfakude30@users.noreply.github.com>
2024-09-06 15:52:47 +0200

    pagination

--- lambdas/ride/ride.py
+++ lambdas/ride/ride.py
@@ -4,23 +4,44 @@ from boto3.dynamodb.conditions import Key
 import json
 
 RECENT_TRIPS_TABLE = os.getenv('RECENT_TRIPS_TABLE', 'TripsTable')
+PAGE_LIMIT = os.getenv('PAGE_LIMIT', 5)
+
 dynamodb = boto3.resource('dynamodb')
 
 def handler(event, context):
     print(f'My event: {event}')
     
     user_id = event['queryStringParameters']['userId']
-    
+    limit = int(event['queryStringParameters'].get('limit', PAGE_LIMIT))  # Number of items per page
+    last_evaluated_key = event['queryStringParameters'].get('lastEvaluatedKey')  # For pagination
+
     table = dynamodb.Table(RECENT_TRIPS_TABLE)
-    response = table.query(
-        KeyConditionExpression=Key('userId').eq(user_id)
-    )
     
+    # Building the query parameters
+    query_params = {
+        'KeyConditionExpression': Key('userId').eq(user_id),
+        'Limit': limit
+    }
+
+    # Add the ExclusiveStartKey if pagination is being used
+    if last_evaluated_key:
+        query_params['ExclusiveStartKey'] = json.loads(last_evaluated_key)
+
+    # Query the DynamoDB table
+    response = table.query(**query_params)
+
+    # Prepare the result with pagination information
+    result = {
+        'trips': response['Items']
+    }
+
+    # If there are more items to be fetched, include the LastEvaluatedKey
+    if 'LastEvaluatedKey' in response:
+        result['lastEvaluatedKey'] = json.dumps(response['LastEvaluatedKey'])
+
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'recentTrips': response['Items']
-        }),
+        'body': json.dumps(result),
         'headers': {
             'Content-Type': 'application/json',
         }
