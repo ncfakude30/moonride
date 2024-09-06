2d15c15024deb88ed68fb4326ca239ca14745665
ncfakude30 <39239761+ncfakude30@users.noreply.github.com>
2024-09-06 15:43:26 +0200

    again

--- lambdas/ride/ride.py
+++ lambdas/ride/ride.py
@@ -1,26 +1,27 @@
 import os
 import boto3
 from boto3.dynamodb.conditions import Key
-import os
-import boto3 
+import json
 
 RECENT_TRIPS_TABLE = os.getenv('RECENT_TRIPS_TABLE', 'TripsTable')
-
 dynamodb = boto3.resource('dynamodb')
 
 def handler(event, context):
-
     print(f'My event: {event}')
+    
     user_id = event['queryStringParameters']['userId']
-
+    
     table = dynamodb.Table(RECENT_TRIPS_TABLE)
     response = table.query(
         KeyConditionExpression=Key('userId').eq(user_id)
     )
-
+    
     return {
         'statusCode': 200,
-        'body': {
+        'body': json.dumps({
             'recentTrips': response['Items']
+        }),
+        'headers': {
+            'Content-Type': 'application/json',
         }
     }
