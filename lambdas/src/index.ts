3889dff93ac264a7fb910f7ec744d2fcd8148fb9
ncfakude30 <39239761+ncfakude30@users.noreply.github.com>
2024-10-01 17:02:11 +0200

    gol  asjkhbadasjgv  hbad asdg

--- lambdas/src/index.ts
+++ lambdas/src/index.ts
@@ -1,5 +1,6 @@
 // Importing all Lambda handlers
 export { handler as paymentCallBack } from './payment/payment_callback'
+export { handler as payment } from './payment/payment';
 export { handler as trips } from './ride/ride';
 export { handler as login } from './user/login';
 export { handler as user } from './user/user';
@@ -7,6 +8,5 @@ export { handler as paymentRequestId } from './ride/request';
 export { handler as connect } from './websocket/connect';
 export { handler as disconnect } from './websocket/disconnect';
 export { handler as message } from './websocket/message';
-export { handler as payment } from './payment/payment';
 export { handler as directions } from './map/directions';
 export { handler as drivers } from './map/drivers';
--- lambdas/webpack.config.js
+++ lambdas/webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+    },
   },
   module: {
     rules: [
